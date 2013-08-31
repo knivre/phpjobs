71d7f953e3ab368d927890592a2bf140534eeea2
Knivre T <knivre@users.noreply.github.com>
2013-08-31 18:29:27 +0200

    Improved the dummy test job.

--- jobs/test.job.php
+++ jobs/test.job.php
@@ -3,6 +3,7 @@
 	@file test.job.php This is a dummy test job that simply sleeps for 60
 	seconds.
 	The following functions are made available to any job:
+	  * job_start() should be called at the beginning of the treatment
 	  * update_job_state() provides a convenient way to update $job_state_data and
 	sync its contents to the job state file;
 	  * write_error() provides a convenient way to log an error message
@@ -11,11 +12,20 @@
 	  * $job_state_data, the array describing the current job state.
 */
 
-// jobs have to update the state to "running"
-update_job_state(array('state' => 'running'));
+// this will update the adequate state file with start time.
+job_start();
 
+// GET parameters passed to the job manager through HTTP are handed as command
+// line arguments.
 $sleep_time = 60;
 if (isset($arguments['sleep_time']) && preg_match('/^[0-9]+$/', $arguments['sleep_time'])) {
 	$sleep_time = $arguments['sleep_time'];
 }
+
+// example of stderr output
+fprintf(STDERR, "Remember, kids, jobs can write on both stdout and stderr.\n");
+
+// example of stdout output + dummy treatment
+print "About to sleep for ${sleep_time} seconds\n";
 sleep($sleep_time);
+print "Slept for ${sleep_time} seconds\n";
