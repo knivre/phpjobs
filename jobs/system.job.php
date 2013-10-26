cda857b43cd4f6e655f32b2678efd9e8b05f07b9
Knivre T <knivre@users.noreply.github.com>
2013-10-26 21:55:54 +0200

    The "system" job is now able to set and unset environment variables.

--- jobs/system.job.php
+++ jobs/system.job.php
@@ -5,12 +5,14 @@
 // Ensure this file is not executed directly
 if (@constant('JOBS_CONTEXT') != 'worker') exit();
 
+$post_data_restored = restore_post_data();
+
 // require at least one "command" argument before even starting the job
 $command = NULL;
 if (isset($arguments['command']) && strlen(trim($arguments['command']))) {
 	$command = $arguments['command'];
 }
-else if (restore_post_data() && isset($_POST['command']) && strlen(trim($_POST['command']))) {
+else if ($post_data_restored && isset($_POST['command']) && strlen(trim($_POST['command']))) {
 	$command = $_POST['command'];
 }
 if (is_null($command)) {
@@ -20,6 +22,22 @@ if (is_null($command)) {
 }
 
 job_start();
+
+// Take POST environment variables into account
+if ($post_data_restored) {
+	for ($i = 0; ; ++ $i) {
+		$arg_name = 'env' . $i;
+		if (!isset($_POST[$arg_name])) break;
+		if (strlen($_POST[$arg_name])) putenv($_POST[$arg_name]);
+	}
+}
+
+// Take GET environment variables into account
+for ($i = 0; ; ++ $i) {
+	$arg_name = 'env' . $i;
+	if (!isset($arguments[$arg_name])) break;
+	if (strlen($arguments[$arg_name])) putenv($arguments[$arg_name]);
+}
 
 // ensure the most common bin and sbin directories appear in the PATH
 $minimal_path = '/usr/local/bin:/usr/bin:/bin:/sbin:/usr/sbin';
